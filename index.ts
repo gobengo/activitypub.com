bd21abbbcfb21fd07a87e8a5cfc336c72c525356
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-08-15 22:53:01 -0700

    first stab at wwwDomain

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import { Config, getStack, StackReference } from "@pulumi/pulumi";
 
 const stackConfig = new pulumi.Config("activitypub.com-cloud");
 
-let service = new cloud.Service("example", {
+export const service = new cloud.Service("example", {
     containers: {
         nginx: {
             build: "./app",
@@ -24,4 +24,20 @@ exports.url = service.defaultEndpoint.apply(e => `http://${e.hostname}`);
 // exports.loadBalancer = (service.defaultEndpoint as pulumi.Output<cloudAws.Endpoint>).loadBalancer
 
 const apexDnsStack = new StackReference(stackConfig.require("apex-dns-stack"));
-export const apexZone = apexDnsStack.getOutput('apexZone')
+export const apexZone: pulumi.OutputInstance<aws.route53.Zone> = apexDnsStack.getOutput('apexZone')
+export const apexZoneId = apexDnsStack.getOutput('apexZoneId')
+export const apexZoneName = apexDnsStack.getOutput('apexZoneName')
+
+export const wwwRecord = new aws.route53.Record(`www-record`, {
+    type: 'A',
+    name: apexZone.apply(z => `www.${z.name}`),
+    zoneId: apexZone.apply(z => z.id),
+    aliases: [
+        {
+            name: service.defaultEndpoint.apply(e => (e as cloudAws.Endpoint).loadBalancer.dnsName),
+            zoneId: service.defaultEndpoint.apply(e => (e as cloudAws.Endpoint).loadBalancer.zoneId),
+            evaluateTargetHealth: false,
+    
+        }
+    ]
+})
